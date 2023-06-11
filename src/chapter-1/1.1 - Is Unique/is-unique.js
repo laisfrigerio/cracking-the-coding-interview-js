3edd6e037b175de5662f2640171b8adc2be73ea3
Lais Frigerio <lais.frigerio@nubank.com.br>
2023-06-11 12:03:02 -0300

    adding the is unique resolution without clean code aspects

--- src/chapter-1/1.1 - Is Unique/is-unique.js
+++ src/chapter-1/1.1 - Is Unique/is-unique.js
@@ -1,7 +1,16 @@
-const isUniqueWithoutExtraDataStructure = (string) => {
-    return true
+const isUniqueWithoutExtraDataStructure = (s) => {
+  for (let i = 0; i < s.length; i++) {
+    const c = s[i];
+    for (let j = i + 1; j < s.length; j++) {
+      if (c === s[j]) {
+        return false
+      }
+    }
+  }
+
+  return true
 }
 
 module.exports = {
-    isUniqueWithoutExtraDataStructure
+  isUniqueWithoutExtraDataStructure
 }
