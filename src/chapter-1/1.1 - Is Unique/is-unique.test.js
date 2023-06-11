09c868e2e2f3db01994a8859f65c60e50a6cdb70
Lais Frigerio <lais.frigerio@nubank.com.br>
2023-06-11 11:57:35 -0300

    creating a new failed test case

--- src/chapter-1/1.1 - Is Unique/is-unique.test.js
+++ src/chapter-1/1.1 - Is Unique/is-unique.test.js
@@ -4,4 +4,8 @@ describe("Checking string has unique characteres without the usage of extra data
     it("should return true when the given string has unique characteres", () => {
         expect(isUniqueWithoutExtraDataStructure("car")).toBe(true)
     })
+
+    it("should return false when the given string doesn't have unique characteres", () => {
+        expect(isUniqueWithoutExtraDataStructure("banana")).toBe(false)
+    })
 })
